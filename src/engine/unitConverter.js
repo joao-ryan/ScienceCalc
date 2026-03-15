e53d6c90d5e1b7381d4da592bd50cbf59d6d06a8
joao-ryan <joaoryanmaranhaodecastro@gmail.com>
2026-03-15 15:29:44 -0300

    agente de conversão criado

---
+++ src/engine/unitConverter.js
@@ -0,0 +1,11 @@
+export function celsiusToFahrenheit(c){
+  return (c * 9/5) + 32;
+}
+
+export function kmToMiles(km){
+  return km * 0.621371;
+}
+
+export function kgToLb(kg){
+  return kg * 2.20462;
+}
